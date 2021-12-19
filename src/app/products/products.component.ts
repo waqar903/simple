ecc0cc35eedec4ee261a34ba0123ac7a24e2680f
Waqar Khan <k.waqar903@gmail.com>
2021-12-19 14:41:57 +0500

    form

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,9 +8,14 @@ export class ProductsComponent {
   productName = 'A Book';
   products = ['A Book', 'A Tree'];
   constructor() {}
-  addProduct() {
-    this.products.push(this.productName);
+  addProduct(form: any) {
+    // this.products.push(this.productName);
+    if (form.value) {
+      this.products.push(form.value.productName);
+    }
+    console.log(form);
   }
+
   removeProduct(product: string) {
     this.products = this.products.filter((p) => p != product);
   }
